664cec74c34a2c40411db2d7c6b9051c07c06770
umam <infernape_999@hotmail.com>
2023-06-13 02:33:00 +0530

    feat : easy done

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,35 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const uniqueCateg = [];
+  transactions.forEach(obj => {
+   if(!uniqueCateg.includes(obj.category))
+   {
+    uniqueCateg.push(obj.category);
+   }
+  });
+  const expenses = [];
+  uniqueCateg.forEach(categ => {
+    let total = 0;
+    transactions.forEach(obj => {
+      if(categ === obj.category)
+      {
+        total = total + obj.price;
+      }
+    });
+    expenses.push({ "category" : categ, "totalSpent" : total});
+  });
+  console.log(expenses)
+  return expenses;
 }
+// const trans = [
+//   { itemName: "Item 1", category: "Electronics", price: 100, timestamp: "2022-01-01" },
+//   { itemName: "Item 2", category: "Groceries", price: 50, timestamp: "2022-01-02" },
+//   { itemName: "Item 3", category: "Electronics", price: 200, timestamp: "2022-01-03" },
+//   { itemName: "Item 4", category: "Clothing", price: 80, timestamp: "2022-01-04" },
+//   { itemName: "Item 5", category: "Groceries", price: 30, timestamp: "2022-01-05" }
+// ]
+
+// calculateTotalSpentByCategory(trans);
 
 module.exports = calculateTotalSpentByCategory;
