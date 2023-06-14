c1a84bc4b39754582d7c5f330d828d4bed54b832
umam <infernape_999@hotmail.com>
2023-06-14 21:25:53 +0530

    async done

--- 02-async-js/hard (promises)/4-promise-chain.js
+++ 02-async-js/hard (promises)/4-promise-chain.js
@@ -4,19 +4,62 @@
  * Print out the time it takes to complete the entire operation.
  * Compare it with the results from 3-promise-all.js
  */
-
+let startTime = new Date();
 function waitOneSecond() {
-
+    const promise= new Promise((resolve,reject)=>{
+    
+        setTimeout(()=> {
+            resolve(1)
+        }, 1*1000);
+        
+        })
+        
+        return promise;
 }
 
 function waitTwoSecond() {
-
+    const promise= new Promise((resolve,reject)=>{
+    
+        setTimeout(()=> {
+            resolve(2)
+        }, 2000);
+        
+        })
+        
+        return promise;
 }
 
 function waitThreeSecond() {
-
+    const promise= new Promise((resolve,reject)=>{
+    
+        setTimeout(()=> {
+            resolve(3)
+        }, 3000);
+        
+        })
+        
+        return promise;
 }
+
 
 function calculateTime() {
-
+    let one=waitOneSecond();
+    one
+    .then(()=>{
+        console.log("1s")
+        let two = waitTwoSecond();
+        return two;
+    })
+    .then(()=>{
+        console.log("2s");    
+        let three= waitThreeSecond();
+        return three;
+    })
+    .then(()=>{
+        console.log("3s")
+        let endtime = new Date();
+        console.log(endtime-startTime);
+    })
 }
+
+calculateTime();
